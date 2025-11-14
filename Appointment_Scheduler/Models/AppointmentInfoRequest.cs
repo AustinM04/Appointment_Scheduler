18db35b909899efdc17a86afebd7103ea3a240f2
Austin Moore <austinmoore3@mail.weber.edu>
2025-11-13 20:51:09 -0700

    Created appointment info request DTO

---
+++ Appointment_Scheduler/Models/AppointmentInfoRequest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Appointment_Scheduler.Models
+{
+		/// <summary>
+		/// DTO for the appointment info request.
+		/// </summary>
+		public class AppointmentInfoRequest
+		{
+			/// <summary>
+			/// Id of the doctor
+			/// </summary>
+			private int DoctorId { get; set; }
+
+			/// <summary>
+			/// Id of the person who is booking the appointment
+			/// </summary>
+			private int PersonId { get; set; }
+
+			/// <summary>
+			/// Time of the appointment
+			/// </summary>
+			public DateTime AppointmentTime { get; set; }
+
+			/// <summary>
+			/// Boolean indicating if the appointment is for a new patient
+			/// </summary>
+			public bool IsNewPatientAppointment { get; set; }
+
+			/// <summary>
+			/// Id of the info request
+			/// </summary>
+			public int RequestId { get; set; }
+		}
+}
